e39d15390ae575316c8794d0f0a97eae04ac4131
kaavya <kaavyarao05@gmail.com>
2024-10-15 15:49:32 +0530

    feat: playsound function

--- LinkedListPlayer.c
+++ LinkedListPlayer.c
@@ -3,9 +3,14 @@
 #include <windows.h>
 #include <mmsystem.h>
 
-#include "songlist.c"
-
 //g++ LinkedListPlayer.c -lwinmm
+
+#include "songlist.c"
+/*
+songlist.c is a c file in the same folder as LinkedListPlayer.c
+songlist.c contains constants of type LPCSTR
+*/
+
 
 typedef struct SongStruct{
     struct SongStruct * prev;
@@ -13,10 +18,16 @@ typedef struct SongStruct{
     struct SongStruct * next;
 }Song;
 
+void playSound(Song song){
+    PlaySound(TEXT(song.path),NULL,SND_SYNC);
+    //SND_SYNC ensures that the function returns after the song finishes playing
+}
+
 int main(){
-    Song birthdayKid;
-    birthdayKid.path=path1 //from songlist.c
-    PlaySound(TEXT(birthdayKid.path),NULL,SND_ASYNC);
-    system("PAUSE");
+    Song s1;
+    s1.path=PATH1; //from songlist.c
+    playSound(s1);
+    playSound(s1);
+    playSound(s1);
     return 0;
 }
